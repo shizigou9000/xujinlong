1d2612de6ec4a1eda2941474928ccbb54508b667
shizigou9000 <408732818@qq.com>
2017-04-17 21:45:46 +0800

    Update system.js

--- parking/class/system.js
+++ parking/class/system.js
@@ -1,8 +1,53 @@
+var fs = require('fs');
+var parseString = require('xml2js').parseString;
 var system = {}
 system.promptMessage = {
-	'message':'test'
+  'message': 'test'
 };
 system.printReport = function (ceoId, managerId, boyId) {
 
 }
+system.getCharacterList = function (path) {
+  if (fs.existsSync(path)) {
+    fs.readFile(path, function (error, file) {
+      if (!error) {
+        parseString(file, function (err, result) {
+          if (!err) {
+            _formatCharacterList(result);
+          }
+        })
+      }
+    })
+  }
+}
+system.saveData = function (content, path) {
+  fs.writeFile(path, content);
+}
+function _formatCharacterList(result) {
+  var ParkingCEOList = result.ParkingCEO;
+  ParkingCEOList.ParkingManagers.ParkingManager;
+  console.log(ParkingCEOList.ParkingManagers[0].ParkingManager);
+}
+list = [
+  {
+    name: ''
+    ParkingManagers: [
+      {
+        name: '',
+        ParkingBoys: [
+          {
+            name: ''
+            ParkingLots: [
+              {
+                Name: ''
+                Count: ''
+                Capacity: ''
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+]
 module.exports = system;
