0303832ccf14030b05d6a0aa3ab9ad0329eaf0b1
shizigou9000 <408732818@qq.com>
2017-04-18 20:44:54 +0800

    Update app.js

--- parking/app.js
+++ parking/app.js
@@ -1,14 +1,19 @@
 var repl = require('repl');
-var fs = require('fs');
-var parseString = require('xml2js').parseString;
+var path = require('path');
 var config = require('./config.json');
 var system = require('./class/system');
-var pceo = require('./class/pceo');
-var currentDir=__dirname;//当前路径
-var symbol='>';
-var replServer=repl.start({prompt:symbol,eval: init});
-function init(cmd){
-	cmd=cmd.replace('\n','');
-	pceo.addCeo('xjl');
-	replServer.displayPrompt();
+var character = require('./class/character');
+var currentDir = __dirname;
+var symbol = '>';
+var replServer = repl.start({prompt: symbol, eval: init});
+function init(cmd) {
+  cmd = cmd.replace('\n', '');
+  var dataFile = path.join(currentDir, config.dataName);
+  var systemObj = new system(dataFile, config);
+  if (cmd == 'add') {
+    var characterObj = new character(systemObj.characterList, config);
+    systemObj.characterList = (characterObj.addRole('manager', 2));
+    systemObj.saveData();
+  }
+  replServer.displayPrompt();
 }
