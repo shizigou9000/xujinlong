958ac1a7784f15f0bc2594901627c42beaddf967
shizigou9000 <408732818@qq.com>
2017-04-19 07:59:23 +0800

    Update app.js

--- parking/app.js
+++ parking/app.js
@@ -10,10 +10,14 @@ function init(cmd) {
   cmd = cmd.replace('\n', '');
   var dataFile = path.join(currentDir, config.dataName);
   var systemObj = new system(dataFile, config);
+  var characterObj = new character(systemObj.characterList, config);
   if (cmd == 'add') {
-    var characterObj = new character(systemObj.characterList, config);
     systemObj.characterList = (characterObj.addRole('manager', 2));
     systemObj.saveData();
+  }else if(cmd=='parking'){
+	var carNumber='123456';
+	var parkingInfo=characterObj.parking(carNumber);
+	console.log(parkingInfo);
   }
   replServer.displayPrompt();
 }
